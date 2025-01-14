cfa15fafd299cbe6c02754941cee3475897a4d94
victorustmar <150936540+victorustmar@users.noreply.github.com>
2025-01-14 19:27:12 +0200

    Delete sequelize.js

--- sequelize.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize, Op } from "sequelize";
-
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./database.db", 
-    logging: console.log,
-});
-export { sequelize, Op };
-export default sequelize;
