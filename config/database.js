b65347e1e5b279a3d6eb1667a1ea444250a582aa
victorustmar <150936540+victorustmar@users.noreply.github.com>
2025-01-14 19:26:43 +0200

    Delete config directory

--- config/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./database.db", 
-    logging: console.log,
-});
-
-export default sequelize;
